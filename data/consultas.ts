03a2e8de865fa694dbd4a9f31fe2d5cfd524bb0c
Matheusydev <matheusylanaraujo@gmail.com>
2026-05-16 16:24:46 -0300

    chore: adicionando mais consultas

--- data/consultas.ts
+++ data/consultas.ts
@@ -29,4 +29,46 @@ export const consultas: Consulta[] = [
         especialidade: "Nutricionista",
         localizacao: "Zona Sul"
     },
+        {
+        id: "5",
+        data: new Date("2026-05-27T16:00:00"),
+        medico: "Dr. Felipe Andrade",
+        especialidade: "Dermatologista",
+        localizacao: "Centro"
+    },
+    {
+        id: "6",
+        data: new Date("2026-05-28T08:30:00"),
+        medico: "Dra. Camila Nogueira",
+        especialidade: "Ginecologista",
+        localizacao: "Zona Leste"
+    },
+    {
+        id: "7",
+        data: new Date("2026-05-28T11:00:00"),
+        medico: "Dr. Lucas Ferreira",
+        especialidade: "Ortopedista",
+        localizacao: "Zona Norte"
+    },
+    {
+        id: "8",
+        data: new Date("2026-05-29T13:45:00"),
+        medico: "Dra. Juliana Costa",
+        especialidade: "Endocrinologista",
+        localizacao: "Zona Sul"
+    },
+    {
+        id: "9",
+        data: new Date("2026-05-30T10:20:00"),
+        medico: "Dr. André Lima",
+        especialidade: "Urologista",
+        localizacao: "Centro"
+    },
+    {
+        id: "10",
+        data: new Date("2026-05-31T15:10:00"),
+        medico: "Dra. Mariana Alves",
+        especialidade: "Neurologista",
+        localizacao: "Zona Leste"
+    }
 ];
